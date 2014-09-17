5973d00adb27709cfdeec64592677f3a4737dda5
Daniel M. Campos Carranza <manic988@hotmail.com>
2014-09-17 22:38:04 +0200

    Preparadas las URLs para cada tarea

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,15 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    //Ok(views.html.index("Your new application is ready."))
+    Ok("Hello World")
   }
 
+  def index = Action {
+   Redirect(routes.Application.tasks)
+  }
+
+  def tasks = TODO
+  def newTask = TODO
+  def deleteTask(id: Long) = TODO
 }
